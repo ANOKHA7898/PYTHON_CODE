e129b53461cf818a8315b9a7995347ad1b60d701
Anokha <[Danokhat@gmail.com>
2024-08-30 11:05:50 +0530

    code modified for  checking number is palindrome

--- Check_palindrome.py
+++ Check_palindrome.py
@@ -8,3 +8,9 @@ while num > 0:
     num = num // 10
 
 print("The Reverse of the number is: ", rev)
+
+if rev == temp_num:
+    print("Entered Number : ",temp_num,"is Palindrome")
+else:
+    print("Entered Number : ",temp_num,"is Not Palindrome")
+    
