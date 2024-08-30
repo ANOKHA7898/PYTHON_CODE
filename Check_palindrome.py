a064b8beba04ac2605ab76b8e5fa967d1777f02c
Anokha <[Danokhat@gmail.com>
2024-08-30 15:35:48 +0530

    adding description about code

--- Check_palindrome.py
+++ Check_palindrome.py
@@ -1,6 +1,11 @@
-num = int(input("Enter a Number: "))
+# this python code is to check the entered number is palindrome or not
+
+
+num = int(input("Enter a Number: ")) #taking input from the user
 temp_num = num
-rev = 0
+rev = 0                              # intial value of reverseing number
+
+#logic of the program
 while num > 0:
     rev = rev * 10 + (num %10)
     num = num // 10
