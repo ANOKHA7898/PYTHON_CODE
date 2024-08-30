635dd992ae9ed0f39f1b5f6967ea211624996187
Anokha <[Danokhat@gmail.com>
2024-08-30 11:06:32 +0530

    code modified for  checking number is palindrome

--- Check_palindrome.py
+++ Check_palindrome.py
@@ -1,8 +1,6 @@
 num = int(input("Enter a Number: "))
 temp_num = num
 rev = 0
-
-
 while num > 0:
     rev = rev * 10 + (num %10)
     num = num // 10
@@ -13,4 +11,3 @@ if rev == temp_num:
     print("Entered Number : ",temp_num,"is Palindrome")
 else:
     print("Entered Number : ",temp_num,"is Not Palindrome")
-    
